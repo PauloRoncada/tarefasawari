74383e8164b6b3930028b64185590f44830a2582
PauloRC <paulorc_@hotmail.com>
2023-08-02 05:51:00 -0400

    teste

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+//programa que recebe num inteiro e mostra contagem do 1 até o num.
+
+const target = window.prompt('Digite um número inteiro:', '10');
+
+for (let i=1; i<=target; i++) {
+    console.log(i);
+}
---
+++ exercicio6/script.js
@@ -0,0 +1,9 @@
+// 6. Programa que recebe um número inteiro e mostre a tabuada desse número.
+
+const target = window.prompt('Digite um número inteiro:', '5');
+
+console.log(`Tabuada de ${target}`);
+for (let i=1; i<=10; i++) {
+    const result = target * i;
+    console.log(`${target} x ${i} = ${result}`);
+}
